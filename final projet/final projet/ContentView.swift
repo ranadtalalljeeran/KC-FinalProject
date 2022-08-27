ecbff11387db58f226910915a504cbae4726e98a
ranadtalalljeeran <ranadtalalaljiran@gmail.com>
2022-08-27 13:13:43 +0300

    .

---
+++ final projet/final projet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  final projet
+//
+//  Created by Talal Aljiran AlJiran KWI-8800 on 27/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ final projet/final projet/final_projetApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_projetApp.swift
+//  final projet
+//
+//  Created by Talal Aljiran AlJiran KWI-8800 on 27/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct final_projetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
